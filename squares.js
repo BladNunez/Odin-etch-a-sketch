8960d5c9fc2b243cf738fd874f3bf61cda5bcbb4
BladNunez <Nunez.Bladimir@yahoo.com>
2023-02-26 18:55:33 -0500

    Create 16 divs using DOM

---
+++ squares.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+
+//create 16x16 square divs before adding to parent container 
+let divOne = document.createElement('div');
+divOne.classList.add('1');
+
+container.appendChild(divOne);
+
+let divTwo = document.createElement('div');
+let divThree = document.createElement('div');
+let divFour = document.createElement('div');
+let divFive = document.createElement('div');
+let divSix = document.createElement('div');
+let divSeven = document.createElement('div');
+let divEight = document.createElement('div');
+let divNine = document.createElement('div');
+let divTen = document.createElement('div');
+let divEleven = document.createElement('div');
+let divTwelve = document.createElement('div');
+let divThirteen = document.createElement('div');
+let divFourteen = document.createElement('div');
+let divFifteen = document.createElement('div');
+let divSixteen = document.createElement('div');
+
