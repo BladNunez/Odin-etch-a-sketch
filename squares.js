1ed41931a8ec5c0d9ebf9818b61cb928a604e81c
BladNunez <Nunez.Bladimir@yahoo.com>
2023-02-26 20:09:28 -0500

    refactor code to loops to create 16x16 grid

--- squares.js
+++ squares.js
@@ -1,67 +1,15 @@
 const container = document.querySelector('.container');
 
 //create 16x16 square divs before adding to parent container 
-let divOne = document.createElement('div');
-divOne.classList.add('1');
-container.appendChild(divOne);
+for(let i = 0; i < 16; i++){
+        for(let j = 0; j < 16; j++){
+                const squares = document.createElement('div');
+                container.appendChild(squares);
+        }
+}
+const divs = container.querySelectorAll('div');
 
-let divTwo = document.createElement('div');
-divTwo.classList.add('2');
-container.appendChild(divTwo);
-
-let divThree = document.createElement('div');
-divThree.classList.add('3');
-container.appendChild(divThree);
-
-let divFour = document.createElement('div');
-divFour.classList.add('4');
-container.appendChild(divFour);
-
-let divFive = document.createElement('div');
-divFive.classList.add('5');
-container.appendChild(divFive);
-
-let divSix = document.createElement('div');
-divSix.classList.add('6');
-container.appendChild(divSix);
-
-let divSeven = document.createElement('div');
-divSeven.classList.add('7');
-container.appendChild(divSeven);
-
-let divEight = document.createElement('div');
-divEight.classList.add('8');
-container.appendChild(divEight);
-
-let divNine = document.createElement('div');
-divNine.classList.add('9');
-container.appendChild(divNine);
-
-let divTen = document.createElement('div');
-divTen.classList.add('10');
-container.appendChild(divTen);
-
-let divEleven = document.createElement('div');
-divEleven.classList.add('11');
-container.appendChild(divEleven);
-
-let divTwelve = document.createElement('div');
-divTwelve.classList.add('12');
-container.appendChild(divTwelve);
-
-let divThirteen = document.createElement('div');
-divThirteen.classList.add('13');
-container.appendChild(divThirteen);
-
-let divFourteen = document.createElement('div');
-divFourteen.classList.add('14');
-container.appendChild(divFourteen);
-
-let divFifteen = document.createElement('div');
-divFifteen.classList.add('15');
-container.appendChild(divFifteen);
-
-let divSixteen = document.createElement('div');
-divSixteen.classList.add('16');
-container.appendChild(divSixteen);
+divs.forEach((div) =>{
+        div.style.cssText = 'border-style: solid; border-color: red;'
+});
 
